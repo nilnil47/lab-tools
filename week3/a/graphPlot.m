424f63075d97fabd2bda94cf6a658419737aa9cd
nilnil47 <nilnil47@gmail.com>
2022-05-06 15:54:58 +0300

    update week 3 a

--- week3/a/graphPlot.m
+++ week3/a/graphPlot.m
@@ -1,3 +1,12 @@
+name = "self_time_ax";
+eval(sprintf("mkdir %s",name));
+hold on
+fittedmodel = fit_r_t_ax;
+
+set(gca,'FontSize',30);
+
+%Add labels:
+
 %Set font sizes:
 hold on
 errorbar(R,T,Terror,Terror,Rerror,Rerror,LineStyle="none") 
@@ -8,4 +17,38 @@ xlabel('Resistance (Oham) ','FontSize',20);
 ylabel('Self Timing (s)','FontSize',20);
 
 % Add title:
-title('Self Timing as Function of Resistence', 'FontSize',35);
+legend({'included points','excluded points'})
+
+grid on;
+grid minor;
+
+
+saveas(gca, sprintf("%s/%s.png", name, name), "png");
+saveas(gca, sprintf("%s/%s.fig", name, name), "fig");
+saveas(gca, sprintf("%s/%s.svg", name, name), "svg");
+
+
+
+errors = calcErrorMain(fittedmodel)
+generateResults(fittedmodel, errors, name)
+
+
+
+
+
+
+
+
+
+
+% %Set font sizes:
+% hold on
+% errorbar(R,T,Terror,Terror,Rerror,Rerror,LineStyle="none") 
+% set(gca,'FontSize',30);
+
+% %Add labels:
+% xlabel('Resistance (Oham) ','FontSize',20);
+% ylabel('Self Timing (s)','FontSize',20);
+
+% % Add title:
+% title('Self Timing as Function of Resistence', 'FontSize',35);
